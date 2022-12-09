f54dc7be05a2dd5cad69f156d8ab43715901fac8
Funbucket <77572484+Funbucket@users.noreply.github.com>
2022-12-09 20:04:43 +0900

    Fix: Update vacation.py
    방학 기간 수정

--- cnuchatbot-fastapi/common/vacation.py
+++ cnuchatbot-fastapi/common/vacation.py
@@ -1,7 +1,7 @@
 from datetime import date
 
-vacation_start = date(2022, 6, 22)
-vacation_end = date(2022, 9, 1)
+vacation_start = date(2022, 12, 21)
+vacation_end = date(2023, 3, 1)
 
 
 def is_vacation():
